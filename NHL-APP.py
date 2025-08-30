6a1348bea373c57ac208e62f81bede6b909f1ab3
mtshaliqiniso50-svg <mtshaliqiniso50@gmail.com>
2025-08-30 07:13:31 +0200

    Update NHL-APP.py

--- NHL-APP.py
+++ NHL-APP.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import pickle as pkl
+import pickle 
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
